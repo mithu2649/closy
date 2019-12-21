8c0edaacfd439d7714eae4536b35c3c8288d44e6
mithu2649 <mithu2649@gmail.com>
2019-12-21 18:51:32 +0530

    Change password added

--- change_password.php
+++ change_password.php
@@ -1,6 +1,7 @@
 <?php 
     include('classes/DB.php');
     include('classes/Login.php');
+    $tokenIsValid = False;
 
 
     if(Login::isLoggedIn()){
@@ -24,13 +25,42 @@
             }
         }
     }else{
+        if(isset($_GET['token'])){
+            $token = $_GET['token'];
+            if(DB::query('SELECT user_id FROM password_tokens WHERE token=:token', array(':token'=>sha1($token)))){
+                $user_id = DB::query('SELECT user_id FROM password_tokens WHERE token=:token', array(':token'=>sha1($token)))[0]['user_id'];
+                $tokenIsValid = True;
+                if(isset($_POST['changepassword'])){
+                    $new_password =  $_POST['newpassword'];
+                    $new_password_repeat =  $_POST['newpasswordrepeat'];
+                    
+                        if($new_password == $new_password_repeat){
+                            if(strlen($new_password)>=6 && strlen($new_password)<=60){
+                                DB::query('UPDATE users SET password=:newpassword WHERE id=:user_id', array(':newpassword'=>password_hash($new_password, PASSWORD_BCRYPT), ':user_id'=>$user_id));
+                                echo 'password_changed_succesfully';
+
+                                DB::query('DELETE FROM password_tokens WHERE user_id=:user_id', array(':user_id'=>$user_id));
+                            }
+                        }else{
+                            echo 'passwords_dont_match';
+                        }
+                }
+            }else{
+                die('invalid_token');
+            }
+        }else{      
         die('not_logged_in');
     }
+}
 ?>
 
-<form action="change_password.php" method="post">
+<form action="<?php echo (!$tokenIsValid ? 'change_password.php' : 'change_password.php?token='.$token.'');  ?>" method="post">
     <h2>Change Password</h2>
-    <input type="password" name="currentpassword" id="currentpassword" placeholder="current password"><br>
+    <?php
+        if(!$tokenIsValid){
+            echo '<input type="password" name="currentpassword" id="currentpassword" placeholder="current password"><br>';
+        }
+    ?>
     <input type="password" name="newpassword" id="newpassword" placeholder="new password"><br>
     <input type="password" name="newpasswordrepeat" id="newpasswordrepeat" placeholder="repeat new password"><br>
     <input type="submit" name="changepassword" value="change password">
