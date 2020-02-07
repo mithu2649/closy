355f15d71082dcd1b203c4e075d504ed717855cc
mithu2649 <mithu2649@gmail.com>
2020-02-08 01:55:54 +0530

    Adds Timeline/Feed - users can view updates of people they follow

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php 
     include('classes/DB.php');
     include('classes/Login.php');
-    
+    $showTimeline = False;
     if(Login::isLoggedIn()){
-        echo 'logged_in with userID: ';
-        echo Login::isLoggedIn();
+        echo 'logged_in with userID: '.Login::isLoggedIn().'<br><br>';
+        $showTimeline = True;
     }else{
         echo 'not_logged_in';
     }
+
+    $followingPosts = DB::query('SELECT posts.body, posts.likes, users.username FROM users, posts, followers
+    WHERE posts.user_id = followers.user_id 
+    AND users.id = posts.user_id
+    AND follower_id = 3
+    ORDER BY posts.likes DESC;');
+
+    foreach($followingPosts as $post){
+        echo $post['body'].' ~ '.$post['username'].'<hr>';
+    }
+
 ?>
